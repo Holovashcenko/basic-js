a9546c7e087f635cf77bea873899955c3f8b2a0b
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-12 16:26:09 +0200

    feat: realized createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  const command = [];
+  if (members) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == "string") command.push(members[i]);
+    }
+  }
+  console.log(command);
+  if (command.length != 0) {
+    return command
+      .map((j) => j.trim())
+      .map((e) => e.charAt(0))
+      .join("")
+      .toUpperCase()
+      .split("")
+      .sort()
+      .join("");
+  } else {
+    return false;
+  }
 };
