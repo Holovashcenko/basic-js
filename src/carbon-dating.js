1a83116c4dd34980185f3712ff86aee8bdba526d
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-12 15:12:22 +0200

    feat: realized dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  console.log(typeof sampleActivity);
+  if (typeof sampleActivity === "string" && typeof +sampleActivity !== "number") {
+    return Math.floor(
+      Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+    );
+  } else {
+    return false;
+  }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(arr) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let arrCats = [];
+  let cats = [];
+  for (let i = 0; i < arr.length; i++)
+    arrCats.push(arr[i].filter((e) => e == "^^"));
+
+  for (let j = 0; j < arrCats.length; j++)
+    for (let n = 0; n < arrCats.length; n++)
+      if (arrCats[j][n] == "^^") cats.push(arrCats[j][n]);
+
+  return cats.length;
 };
