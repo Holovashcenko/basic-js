1a6459311d4f6783d247368b6c151a93a175e15e
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-15 14:12:10 +0200

    feat: realized calculateHanoi function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  console.log(arr);
+  const words = [
+    `--discard-next`,
+    `--discard-prev`,
+    `--double-next`,
+    `--double-prev`,
+  ];
+
+  switch (arr) {
+    case words[0]:
+      arr.
+      break;
+    case words[1]:
+      break;
+    case words[2]:
+      break;
+    case words[3]:
+      break;
+    default:
+      break;
+  }
 };
